6612bcde0023165e55392342cfa7c12da72fdc4c
daniel3g <silvadaniel2006@yahoo.com.br>
2026-03-09 12:14:07 -0300

    criei a função media

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const numero1 = 8
-const numero2 = 12
-
-function somaNumeros(){
-    console.log("Cheguei aqui")
+function somaNumeros(a, b){
+    return a + b
 }
 
-somaNumeros()
+const soma1 = somaNumeros(8, 15)
+const soma2 = somaNumeros(15, 17)
+
+console.log(soma1)
+console.log(soma2)
+
+//criar um arquivo media.js e nele uma função que recebe
+// dois valores e calcula a média deles, 
+// retorna a média e exibe no console.log
